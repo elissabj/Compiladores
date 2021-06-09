0c8b56f530ddd26445edd4fc10911b48be82920b
elissabj <56558029+elissabj@users.noreply.github.com>
2021-06-09 14:26:04 -0500

    Add files via upload

---
+++ Proyecto Final/lexerS/lexer.py
@@ -0,0 +1,116 @@
+from ply import *
+import sys
+
+first = []
+symbolTable = {}
+literals = [',','.']
+reserved = {'signal':'TK_SGN', 'float':'TK_FLT', 'interpolate':'TK_OPEI', 'decimate':'TK_OPED', 'shift':'TK_OPES', 'reflect':'TK_OPER', 'amplitude':'TK_OPEA'}
+tokens = list(reserved.values()) + ['TK_NAMEF','TK_NAME', 'TK_LNBK', 'TK_DATAS', 'TK_DATAI', 'TK_ASIG', 'TK_LCOR', 'TK_RCOR', 'TK_LPAR', 'TK_RPAR']
+
+def t_TK_SGNA(t):
+    r'signal'
+    symbolTable["TK_SGN"] = 'signal'
+    first.append('signal')
+    t.type = reserved.get(t.value)
+    return t
+
+def t_TK_FLTA(t):
+    r'float'
+    symbolTable["TK_FLT"] = 'float'
+    first.append('float')
+    t.type = reserved.get(t.value)
+    return t
+
+def t_TK_DATAI(t):
+    r'\-*\d+\.*\d*'
+    t.value = float(t.value)
+    symbolTable["TK_DATAI"] = float(t.value)
+    return t
+
+def t_TK_OPEI(t):
+    r'interpolate'
+    t.type = reserved.get(t.value)
+    symbolTable["TK_OPE"] = 'interpolate'
+    return t
+
+def t_TK_OPED(t):
+    r'decimate'
+    t.type = reserved.get(t.value)
+    symbolTable["TK_OPE"] = 'decimate'
+    return t
+
+def t_TK_OPES(t):
+    r'shift'
+    t.type = reserved.get(t.value)
+    symbolTable["TK_OPE"] = 'shift'
+    return t
+
+def t_TK_OPER(t):
+    r'reflect'
+    t.type = reserved.get(t.value)
+    symbolTable["TK_OPE"] = 'reflect'
+    return t
+
+def t_TK_OPEA(t):
+    r'amplitude'
+    t.type = reserved.get(t.value)
+    symbolTable["TK_OPE"] = 'amplitude'
+    return t
+
+def t_TK_DATAS(t):
+    r'[a-zA-Z_][a-zA-Z]*\.wav'
+    symbolTable["TK_DATA"] = t.value
+    return t
+
+def t_TK_NAME(t):
+    r'[a-zA-Z_][a-zA-Z0-9_]*'
+    first.append(t.value)
+    return t
+
+def t_TK_NAMEF(t):
+    r'[a-z][a-z]*'
+    first.append(t.value)
+    return t
+
+t_TK_SGN = t_TK_SGNA
+t_TK_FLT = t_TK_FLTA
+t_TK_ASIG = r'='
+t_TK_LNBK = r';'
+t_TK_LCOR = r'\['
+t_TK_RCOR = r'\]'
+t_TK_LPAR = r'\('
+t_TK_RPAR = r'\)'
+
+def t_error(t):
+    print("Illegal Symbol '%s'" %t.value[0])
+    t.lexer.skip(1)
+    sys.exit()
+
+
+lex.lex(debug=0)
+          
+
+class Information:
+    def __init__(self, dict):
+        self.dict = dict
+
+    def getDict(self):
+        return self.dict
+
+class Details:
+    def __init__(self, first):
+        self.first = first
+    
+    def getFirst(self):
+        return self.first
+
+def conection():
+    info = Information(symbolTable)
+    symbols = info.getDict()
+    return symbols
+
+def conectionDetails():
+    details = Details(first)
+    possibleError = details.getFirst()
+    return possibleError
+
---
+++ Proyecto Final/parseS/parse.py
@@ -0,0 +1,83 @@
+from ply import *
+import sys
+import lexerS.lexer as lx
+
+symbolsTable = lx.conection()
+tokens = lx.tokens
+
+def line(p):
+    '''
+    line : expression
+    '''
+    pass
+
+def p_expression(p):
+    '''
+    expression : declaration declaration declarationFuntion TK_LNBK
+    '''
+    pass
+
+def p_declaration(p):
+    '''
+    declaration : declareS 
+    | declareI
+    '''
+    pass
+
+def p_declareS(p):
+    '''
+    declareS : TK_SGN TK_LCOR TK_NAME TK_RCOR TK_ASIG TK_DATAS TK_LNBK
+    '''
+    pass
+
+def p_declareI(p):
+    '''
+    declareI : TK_FLT TK_LCOR TK_NAME TK_RCOR TK_ASIG TK_DATAI TK_LNBK
+    '''
+    pass
+
+def p_declarationFuntion(p):
+    '''
+    declarationFuntion : declareBasicOp 
+    | declareOp
+    '''
+    pass
+
+def p_declareBasicOp(p):
+    '''
+    declareBasicOp : TK_OPER TK_LPAR TK_NAME TK_RPAR 
+    '''
+    pass
+
+def p_typeOperation(p):
+    '''
+    typeOperation : TK_OPED
+    | TK_OPES
+    | TK_OPEI
+    | TK_OPEA
+    '''
+    pass
+def p_declareOp(p):
+    '''
+    declareOp : typeOperation TK_LPAR TK_NAME ',' TK_NAME TK_RPAR 
+    '''
+    pass
+
+def p_error(p): 
+    global s
+    if p:
+        print("Syntax Error '%s'" %p.value)
+        sys.exit()
+    else: 
+        print("EOF Error")
+        sys.exit()
+
+
+sParser = yacc.yacc()
+
+def sgnParser(code):
+    sParser.error = 0
+    p = sParser.parse(code)
+    if sParser.error:
+        return None
+    return p  
